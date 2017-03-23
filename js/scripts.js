606d670db36491b213fb8e65374c5a1cbca41e5e
Andrew Dalton and Jessica Robertson <jessica@>
2017-03-22 20:55:44 -0700

    get our golldern survey to successfully display one of three pieces of advice to the user depending on their answers as scored by our scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,40 @@
 
+var stressLevel = 0
+
 $(document).ready(function(){
   $("form#stress-test").submit(function(event){
     event.preventDefault();
     $("#stress-signs").show();
     $("input:checkbox[name=signs]:checked").each(function(){
       var yourSigns = $(this).val();
-      console.log(yourSigns);
+      stressLevel = stressLevel +=2;
       $("#stress-signs").append(yourSigns + "<br>");
     });
     $("#stress-symptoms").show();
     $("input:checkbox[name=symptoms]:checked").each(function(){
       var yourSymptoms = $(this).val();
-      console.log(yourSymptoms);
+      stressLevel = stressLevel +=3;
       $("#stress-symptoms").append(yourSymptoms + "<br>");
     });
     $("#coping-mechanisms").show();
     $("input:checkbox[name=coping-methods]:checked").each(function(){
       var yourCopingMethods = $(this).val();
-      console.log(yourCopingMethods);
+      stressLevel = stressLevel -=2;
       $("#coping-mechanisms").append(yourCopingMethods + "<br>");
     });
     $("#stress-test").hide();
+    if (stressLevel >= -10 && stressLevel <= 10) {
+      $("#make-changes").hide();
+      $("#doomed").hide();
+      $("#good-job").show();
+    } else if (stressLevel >= 11 && stressLevel <= 19) {
+      $("#good-job").hide();
+      $("#doomed").hide();
+      $("#make-changes").show();
+    } else if (stressLevel >= 20 && stressLevel <= 25) {
+      $("#make-changes").hide();
+      $("#good-job").hide();
+      $("#doomed").show();
+    };
   });
 });
